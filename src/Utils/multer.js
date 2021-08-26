4d9c2f2b6ae6511be529e944b94d0c18889f7701
MoacirMSJ <moacirmascarenha86@gmail.com>
2021-08-26 11:06:46 -0300

    Added lib multer and created multer config.

---
+++ src/Utils/multer.js
@@ -0,0 +1,40 @@
+const multer = require('multer');
+const path = require('path');
+const crypto = require('crypto');
+
+
+module.exports ={
+    storage: multer.diskStorage({
+
+        destination: (req, file, cb) =>{
+            cb(null, path.resolve(__dirname, '..', '..', 'files','uploads'));
+        },
+        filename: (req, file, cb) =>{
+            crypto.randomBytes(16,(err, hash)=> {
+                if(err){
+                    cb(err);
+                }else{
+                    const fileName = `${hash.toString('hex')}-${file.originalname}`;
+                    cb(null, fileName);
+                }
+
+            });
+        }
+    }),
+    limits:{
+        // 5 MB max size
+        fileSize: 5 * 1024 * 1024,
+    },
+    fileFilter: (req, file, cb) => {
+        const allowedFormat = [
+            "application/pdf",
+        ];
+
+        if(allowedFormat.includes(file.mimetype)){
+            cb(null, true);
+        }else{
+            cb(new Error('Invalid format.'))
+        }
+    },
+
+};
