c32e4c812e74e52825c0d00a874b740ca5ab5db8
MoacirMSJ <moacirmascarenha86@gmail.com>
2021-09-28 01:35:42 -0300

    Adicionada rota get para o arquivos.

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ const CategoryController = require('./Controllers/CategoryController');
 const DemandController = require('./Controllers/DemandController');
 const AlertController = require('./Controllers/AlertController');
 const multer = require('multer');
-const multerConfig =require('./Utils/multer');
+const multerConfig = require('./Utils/multer');
 
 routes.get('/category', verifyJWT, CategoryController.categoryGet);
 routes.get('/category/:id', verifyJWT, CategoryController.categoryId);
@@ -25,7 +25,8 @@ routes.get('/clientsNames', verifyJWT, DemandController.demandGetWithClientsName
 routes.put('/demand/create-demand-update/:id', verifyJWT, DemandController.createDemandUpdate);
 routes.put('/demand/update-demand-update/:id', verifyJWT, DemandController.updateDemandUpdate);
 routes.put('/demand/delete-demand-update/:id', verifyJWT, DemandController.deleteDemandUpdate);
-routes.post('/demand/upload-file/:id', verifyJWT, multer(multerConfig).single("file"), DemandController.uploadFile);
+routes.post('/demand/upload-file/:id', verifyJWT, multer(multerConfig).single('file'), DemandController.uploadFile);
+routes.get('/demand/file/:idFile', DemandController.getFile);
 routes.get('/demand/history/:id', verifyJWT, DemandController.history);
 routes.get('/statistic/category', verifyJWT, DemandController.demandsCategoriesStatistic);
 routes.get('/statistic/sector', verifyJWT, DemandController.demandsSectorsStatistic);
